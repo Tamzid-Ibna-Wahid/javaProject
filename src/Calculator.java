239141d4020d616c0dbc2095f0023b9b537690f5
Tamzid <tamzidaraf2015@gmail.com>
2024-05-01 12:36:08 +0600

    add

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,30 @@
 public class Calculator {
 
-    public double divide(double d1, double d2) {
+    public static double divide(double d1, double d2) {
         return d1 / d2;
     }
 
-    public double sum(double firstnumber, double secondnumber) {
+    public static  double sum(double firstnumber, double secondnumber) {
         return firstnumber + secondnumber;
     }
 
-    public double multiply(double first, double second) {
+    public static double multiply(double first, double second) {
         return first * second;
     }
 
-    public double substraction(double first, double second) {
+    public static double substraction(double first, double second) {
         return first * second;
+    }
+
+    public static void main(String[] args) {
+
+        double a=40 , b=50;
+
+        System.out.println("Sum = "+sum(a,b));
+        System.out.println("Substraction = "+substraction(a,b));
+        System.out.println("Multiply = "+multiply(a,b));
+        System.out.println("Divition = "+divide(a,b));
+
     }
     
 }
